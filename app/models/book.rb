0939929edf7529edadde93e5a0ad2af97e6f5f36
bsp3ars <bsp3ars@gmail.com>
2018-09-10 15:32:40 -0400

    Handling multivalued author fields

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ActiveFedora::Base
   property :title, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
     index.as :stored_searchable
   end
-  property :author, predicate: ::RDF::Vocab::DC.creator, multiple: false do |index|
+  property :author, predicate: ::RDF::Vocab::DC.creator, multiple: true do |index|
     index.as :stored_searchable
   end
 end
